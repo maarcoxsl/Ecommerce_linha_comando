8d44944341d36540c7a3a95d5d97f20a863e4879
marcosxsl <marcolima.05@outlook.com>
2026-08-15 18:40:28 -0300

    e_commerce V6

--- comercio_eletronico.py
+++ comercio_eletronico.py
@@ -1,19 +1,49 @@
-print('Progama iniciado !!')
+# Back end de ecommerce 
 while True:
+    print('Progama iniciado !!')
+    Iniciar = input('Digite i para encerrar o progama ou qualquer comando para prosseguir: ')
+    if Iniciar == 'i':
+       print('Progama encerrado')
+       break
     print('Bem vindo ao ecommerce-X')
-    cadastro = input('Insira o seu nome para iniciar o progama ').strip()
-    print(f'Olá {cadastro} seja bem vindo ao e commerce-X')
     lista_compra = ['Tv','controle','video game','camisa']
-    print(lista_compra)
-    escolha = input(f'{cadastro}, digite qual item você deseja comprar: ')
-    
-    pesquisa = [escolha for escolha in lista_compra if escolha in lista_compra]
-    print(f'produto escolhido {pesquisa}')
+    print(f'lista de compra {lista_compra}')
+    escolha = input('digite qual item você deseja verificar: ')
 
-    if cadatro == 'parar':
-        print('Progama encerrado')
-        break
-   
+    if escolha in lista_compra[0]:
+       print(f'Produto escolhido: {lista_compra[0]}')
+       Tv_valor = 150.00
+       quantidade = int(input('Quantos produtos você deseja comprar\nEscolha um numeros de 1 a 5'))
+       if quantidade >= 0 and quantidade <= 5:
+        total_compra = quantidade * Tv_valor
+
+        print(f'Valor da compra: {total_compra}')
+        
+        def Pagar(total_compra):
+            metodo_pagamento = input('Como deseja realizar o pagamento desta compra\n obs: pagamentos no cartão com acrescimo de 50 reais ').strip()
+            if metodo_pagamento == 'cartão':
+                conta_final = total_compra + 50
+                print(f'Valor de sua compra: {conta_final}')
+            elif metodo_pagamento == 'dinheiro':
+                 pagamento_dinheiro = total_compra - 50
+            else:
+                print('Formação informada não existe')
+
+              
+
+        Pagar(total_compra)
+        
+
+    elif escolha in lista_compra[1]:
+        print(f'Produto escolhido: {lista_compra[1]}')
+    elif escolha in lista_compra[2]:
+        print(f'Produto escolhido: {lista_compra[2]}')  
+    elif escolha in lista_compra[3]:
+        print(f'Produto escolhido: {lista_compra[3]}')         
+    else:
+        print('produto inexistente')
+    
+    
     
     
        
